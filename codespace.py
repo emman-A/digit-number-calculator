60cf0704d434632b9028b737e0a6ce5cd37c0336
emman-A <emmanuelacheampong869@gmail.com>
2024-11-11 09:53:51 -0600

    first commit

---
+++ codespace.py
@@ -0,0 +1,27 @@
+
+
+
+# def binary_decimal(n):
+#     sum = 0
+#     for i in range(len(n)):
+#         val = int(n[i])
+
+#         if val != 0 and val != 1:
+#             print("Binary value must be 0 or 1")
+#             return None
+            
+#         sum += val * 2**(len(n) - (i+1))
+#     return sum
+# n =  int(input())
+# print(binary_decimal(str(n)))
+
+
+#Decimal to Binary
+def decimal_binary(n):
+    
+print(decimal_binary(39))
+
+
+
+
+    
